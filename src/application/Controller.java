327dda4707fa91ce91a55cc599923467d1e6729b
David <dta31@sfu.ca>
2018-03-22 16:21:53 -0700

    Basic implementation of Copy Pixel
    Basic demo of copy pixel functionality is set up

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -3,12 +3,6 @@ import application.Histogram;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
@@ -37,11 +31,24 @@ public class Controller {
 	private Histogram[] prevFrame;
 	
 	private VideoCapture capture;
-	private ScheduledExecutorService timer;
 	private String fileName;
 	
 	@FXML
-	private void initialize() throws UnsupportedAudioFileException, IOException, LineUnavailableException {	
+	private void initialize() throws IOException {	
+	}
+	
+	@FXML
+	private void selectVideo(ActionEvent event) {
+		try {
+			FileChooser fc = new FileChooser();
+			fc.setInitialDirectory(new java.io.File("."));
+			fc.setTitle("Open Video");
+			File file = fc.showOpenDialog(null);
+			fileName = file.getAbsolutePath();
+		} catch (Exception e) {
+			System.out.println(e);
+			fileName = null;
+		}
 	}
 	
 	@FXML
@@ -53,69 +60,38 @@ public class Controller {
 		
 		System.out.println(stiOption);
 		System.out.println(computationOption);
-	}
-	
-	private String getImageFilename() {
-		// This method should return the filename of the image to be played
-		// You should insert your code here to allow user to select the file
-		FileChooser fc = new FileChooser();
-		fc.setInitialDirectory(new java.io.File("."));
-		fc.setTitle("Open Video");
-		File file = fc.showOpenDialog(null);
-		return file.getAbsolutePath();
-	}
-	
-	@FXML
-	protected void openImage(ActionEvent event) throws InterruptedException {
-		try {
-			fileName = getImageFilename();
-		} catch (Exception e) {
-			System.out.println(e);
-			fileName = null;
-		}
+		
 		if (fileName != null) {
-			closeThreads();
 			capture = new VideoCapture(fileName);
 			if (capture.isOpened()) {
-				createFrameGrabber();
+				scanFrames(stiOption, computationOption);
 			}
 		}
 	}
 	
-	private void createFrameGrabber() throws InterruptedException {
+	private void scanFrames(String stiOption, String computationOption) {
 		if (capture != null && capture.isOpened()) {
-			double framePerSecond = capture.get(Videoio.CAP_PROP_FPS);
-			Runnable frameGrabber = new Runnable() {
-				@Override
-				public void run() {
-					Mat frame = new Mat();
-					if (capture.read(frame)) { // Analyze each frame and compare.
-						Image im = Utilities.mat2Image(frame);
-						Utilities.onFXThread(imageView.imageProperty(), im);
-						double currentFrameNumber = capture.get(Videoio.CAP_PROP_POS_FRAMES);
-						double totalFrameCount = capture.get(Videoio.CAP_PROP_FRAME_COUNT);
-						try {
-							image = frame;
-						} catch (Exception e) {
-							System.out.println(e);
-						}
-					} else {
-						capture.set(Videoio.CAP_PROP_POS_FRAMES, 0);
-					}
-				}
-			};
-			if (timer != null && timer.isShutdown()) {
-				timer.shutdown();
-				timer.awaitTermination(Math.round(1000/framePerSecond), TimeUnit.MILLISECONDS);
+			Mat frame = new Mat();
+			capture.read(frame);
+			int width = frame.width();
+			int height = frame.height();
+			capture.set(Videoio.CAP_PROP_POS_FRAMES, 0);
+			double totalFrames = capture.get(Videoio.CAP_PROP_FRAME_COUNT);
+			image = new Mat(height, (int) totalFrames, frame.type());
+			System.out.println(image.width() + " " + image.height());
+			while(capture.read(frame)) {
+				computeCopyPixel(frame, width, height);
 			}
-			timer = Executors.newSingleThreadScheduledExecutor();
-			timer.scheduleAtFixedRate(frameGrabber, 0, Math.round(1000/framePerSecond), TimeUnit.MILLISECONDS);
+			// Output STI to GUI
+			Image im = Utilities.mat2Image(image);
+            Utilities.onFXThread(imageView.imageProperty(), im);
+			
 		}
 	}
 	
-	protected void closeThreads() {
-		if (timer != null) {
-			timer.shutdownNow();
+	private void computeCopyPixel(Mat frame, int width, int height) {
+		if (frame != null) {
+			(frame.col(width/2)).copyTo(image.col((int) capture.get(Videoio.CAP_PROP_POS_FRAMES) -1));
 		}
 	}
 }
