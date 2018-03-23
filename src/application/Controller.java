22c100fff1c435a2080f5c106a90dedbd3c1b9d1
David <dta31@sfu.ca>
2018-03-23 12:12:36 -0700

    Fully implemented pixel copy
    Implemented pixel copy with downscaling and added demo video for testing

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -12,10 +12,12 @@ import org.opencv.videoio.Videoio;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import utilities.Utilities;
 
@@ -24,6 +26,8 @@ public class Controller {
 	@FXML private ImageView imageView; // the image display window in the GUI
 	@FXML private ToggleGroup sti;
 	@FXML private ToggleGroup computation;
+	@FXML private CheckBox isScaled;
+	@FXML private Text videoName;
 	
 	private Mat image;
 	
@@ -32,6 +36,9 @@ public class Controller {
 	
 	private VideoCapture capture;
 	private String fileName;
+	private int computationOption;
+	private int scaledWidth = 32;
+	private int scaledHeight = 32;
 	
 	@FXML
 	private void initialize() throws IOException {	
@@ -45,6 +52,8 @@ public class Controller {
 			fc.setTitle("Open Video");
 			File file = fc.showOpenDialog(null);
 			fileName = file.getAbsolutePath();
+			String[] filePath = fileName.split("\\\\");
+			videoName.setText("Loaded Video: " + filePath[filePath.length -1]);
 		} catch (Exception e) {
 			System.out.println(e);
 			fileName = null;
@@ -56,31 +65,51 @@ public class Controller {
 		RadioButton stiRadio = (RadioButton) sti.getSelectedToggle();
 		String stiOption = stiRadio.getText();
 		RadioButton computationRadio = (RadioButton) computation.getSelectedToggle();
-		String computationOption = computationRadio.getText();
 		
 		System.out.println(stiOption);
-		System.out.println(computationOption);
+		if (computationRadio.getText().equals("Row")) {
+			computationOption = 0;
+		} else {
+			computationOption = 1;
+		}
 		
 		if (fileName != null) {
 			capture = new VideoCapture(fileName);
 			if (capture.isOpened()) {
-				scanFrames(stiOption, computationOption);
+				scanFrames(stiOption);
 			}
 		}
 	}
 	
-	private void scanFrames(String stiOption, String computationOption) {
+	private void scanFrames(String stiOption) {
 		if (capture != null && capture.isOpened()) {
 			Mat frame = new Mat();
+			Mat scaledFrame = new Mat();
 			capture.read(frame);
-			int width = frame.width();
-			int height = frame.height();
-			capture.set(Videoio.CAP_PROP_POS_FRAMES, 0);
+			int width, height;
+			if (isScaled.isSelected()) {
+				width = scaledWidth;
+				height = scaledHeight;
+			} else {
+				width = frame.width();
+				height = frame.height();
+			}
 			double totalFrames = capture.get(Videoio.CAP_PROP_FRAME_COUNT);
-			image = new Mat(height, (int) totalFrames, frame.type());
-			System.out.println(image.width() + " " + image.height());
+			capture.set(Videoio.CAP_PROP_POS_FRAMES, 0);
+			
+			if (computationOption == 0) {
+				image = new Mat(width, (int) totalFrames, frame.type());
+			} else {
+				image = new Mat(height, (int) totalFrames, frame.type());
+			}
+			
 			while(capture.read(frame)) {
-				computeCopyPixel(frame, width, height);
+				if (isScaled.isSelected()) {
+					Imgproc.resize(frame, scaledFrame, new Size(scaledWidth, scaledHeight));
+					computeCopyPixel(scaledFrame, width, height);
+				} else {
+					computeCopyPixel(frame, width, height);
+				}
 			}
 			// Output STI to GUI
 			Image im = Utilities.mat2Image(image);
@@ -91,7 +120,11 @@ public class Controller {
 	
 	private void computeCopyPixel(Mat frame, int width, int height) {
 		if (frame != null) {
-			(frame.col(width/2)).copyTo(image.col((int) capture.get(Videoio.CAP_PROP_POS_FRAMES) -1));
+			if (computationOption == 0) {
+				(frame.row(height/2)).t().copyTo(image.col((int) capture.get(Videoio.CAP_PROP_POS_FRAMES) -1));
+			} else {
+				(frame.col(width/2)).copyTo(image.col((int) capture.get(Videoio.CAP_PROP_POS_FRAMES) -1));
+			}
 		}
 	}
 }
